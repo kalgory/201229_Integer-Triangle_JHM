79926b4e5eaf633cd8dd2855bf8fc66a5b6aa81d
Hyunmin Jung <jung@hyunmin.dev>
2020-12-23 18:23:36 +0900

    Make prototype

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
 
+using namespace std;
+
+vector<vector<int>> triangle_vec_vec;
+vector<vector<int>> memo_vec_vec;
+int height;
+
+int calculate() {
+  return 0;
+}
+
+int solution(vector<vector<int>> _triangle_vec_vec) {
+  height = _triangle_vec_vec.size();
+  triangle_vec_vec = move(_triangle_vec_vec);
+  memo_vec_vec.resize(height, vector<int>(height, 0));
+
+  int answer = 0;
+  return answer;
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  cout << solution({{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}}) << endl;
   return 0;
 }
